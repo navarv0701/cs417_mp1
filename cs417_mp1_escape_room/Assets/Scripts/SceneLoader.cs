950d8fc18dfff2c87e01d282d371b45737dfa4fc
juliashenn <julias12@illinois.edu>
2026-02-16 19:45:15 -0600

    Changed canvas to xr canvas
    built it on headset and debugged the button and tested grab interactable

--- cs417_mp1_escape_room/Assets/Scripts/SceneLoader.cs
+++ cs417_mp1_escape_room/Assets/Scripts/SceneLoader.cs
@@ -1,28 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // void Start()
     // {
-        
+
     // }
 
     // // Update is called once per frame
     // void Update()
     // {
-        
+
     // }
 
-    public void LoadSceneByName(string sceneName)
+    [SerializeField] private Button button;
+    [SerializeField] private string sceneName;
+
+    void Start()
     {
-        SceneManager.LoadScene(sceneName);
+        button.onClick.AddListener(LoadScene);
     }
 
-    // Function to load a scene by its index (e.g., the second scene is index 1)
-    public void LoadSceneByIndex(int sceneIndex)
+    void LoadScene()
     {
-        SceneManager.LoadScene(sceneIndex);
+        SceneManager.LoadScene(sceneName);
     }
 }
